393f23d0f085f04868f493d8ec0a0238c91d1867
jungeun-p <mrk7753@naver.com>
2021-05-04 22:24:48 +0900

    [ modify ] redux, middleware 적용
    - redux 설치 및 미들웨어 설정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
 import "./App.css";
+import React from "react";
+import { applyMiddleware, createStore } from "redux";
+
+const printLog = (store) => (next) => (action) => {
+  console.log(`prev state = ${JSON.stringify(store.getState())}`);
+  const result = next(action);
+  console.log(`next state = ${JSON.stringify(store.getState())}`);
+  return result;
+};
+
+const myReducer = (state = { name: "mike" }, action) => {
+  console.log("myReducer");
+  if (action.type === "someAction") {
+    return { name: "mikeee" };
+  }
+  return state;
+};
+
+const store = createStore(myReducer, applyMiddleware(printLog));
+store.dispatch({ type: "someAction" });
 
 function App() {
   return <div className="App">Coffee-forest-client</div>;
