61a5eb48634607d7ced9031118ee9a7f1dd1c9ef
jungeun-p <mrk7753@naver.com>
2021-06-20 20:55:03 +0900

    [ create ] Pages/WeeklyWorklist, System/WeeklyAttandacne 컴포넌트 생성
    [ modify ] WeeklyPage 출근, 퇴근 api 연결
    - WeeklyAttendacne -> WeeklyPage로 임시 연결

--- src/Pages/Mypage/WeeklyPage.js
+++ src/Pages/Mypage/WeeklyPage.js
@@ -1,10 +1,50 @@
+import axios from 'axios';
+// @ts-ignore
+import { responseInterceptor } from 'http-proxy-middleware';
 import React from 'react';
+import { LOCAL_HOST } from '../../Lib/constant';
+// import WeeklyAttendance from '../../System/Mypage/WeeklyAttendance';
 import WeeklyData from './Weekly/WeeklyData';
 
 const WeeklyPage = ({ weekend }) => {
+  // 출근 api
+  const onAttandacne = () => {
+    axios
+      .post(`${LOCAL_HOST}attendance`, {
+        companyIndex: weekend.companyIndex,
+        userIndex: weekend.userIndex
+      })
+      .then(response => {
+        alert('출근');
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error.response.data);
+      });
+  };
+
+  // 퇴근 api
+  const onLeaving = () => {
+    axios
+      .patch(`${LOCAL_HOST}attendance`, {
+        companyIndex: weekend.companyIndex,
+        userIndex: weekend.userIndex
+      })
+      .then(response => {
+        alert('퇴근');
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error.response.data);
+      });
+  };
   return (
     <>
       <WeeklyData weekend={weekend} />
+      {/* <WeeklyAttendance weekend={weekend} /> */}
+      <p>이번 주 근무</p>
+      <button onClick={onAttandacne}>출근</button>
+      <button onClick={onLeaving}>퇴근</button>
     </>
   );
 };
