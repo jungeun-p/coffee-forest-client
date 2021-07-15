d87280e09555a08719a1cba1d813f3bb1282021d
jungeun-p <mrk7753@naver.com>
2021-07-15 19:53:14 +0900

    [ modify ] Common/ContentsRoot 반응형 padding값 추가
    - 패드, 웹 환경의 경우 양 옆 padding 10vw 추가
    
    [ modify ] WorkLoad, Plan, Mypage 반응형 변경
    
    - 모바일 혹은 웹일 경우 프로필 상세내역 유무 변경
    - WorkLoad, WorkPlan width값 반응형에 따라 다르게 변경

--- src/Pages/Mypage/Weekly/Data/WorkMypage.js
+++ src/Pages/Mypage/Weekly/Data/WorkMypage.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
-import mainLogo from '../../../../assets/Img/logo/mainLogo.png';
+import mainLogo from '../../../../assets/Img/profile/basicProfile.png';
 
 const WorkMypage = ({ weekend }) => {
   return (
     <MainProfile>
+      <div className="Info">
+        <div className="Name">{weekend.name}</div>
+        <div className="Position">{weekend.position}</div>
+      </div>
       <Logo src={mainLogo} />
-      {/* <div>{weekend.name}</div>
-      <div>{weekend.position}</div> */}
     </MainProfile>
   );
 };
 
-const MainProfile = styled.div``;
+const MainProfile = styled.div`
+  display: flex;
+  flex-direction: row;
+  .Info {
+    visibility: hidden;
+    margin-right: 10px;
+    text-align: right;
+    font-size: 12px;
+    @media all and (min-width: 768px) {
+      visibility: visible;
+    }
+    .Name {
+      font-size: 14px;
+      font-weight: 600;
+    }
+  }
+`;
 
 const Logo = styled.img`
   height: 34px;
