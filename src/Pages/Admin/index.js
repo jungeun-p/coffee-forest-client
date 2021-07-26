a4cbd681dd331bacdc17aaefd24165630bfaa0ea
jungeun-p <mrk7753@naver.com>
2021-07-26 14:47:21 +0900

    [ modify ] 관리자 페이지 styled-component 적용

--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ContainerRoot, ContentsWrapper } from '../../Components/Common';
 import Admin from '../../System/Admin/Admin';
 
 const AdminContainer = () => {
   return (
-    <>
-      <Admin />
-    </>
+    <ContainerRoot>
+      <ContentsWrapper>
+        <AdminWrap>
+          <div className="box">
+            <Admin />
+          </div>
+        </AdminWrap>
+      </ContentsWrapper>
+    </ContainerRoot>
   );
 };
+
+const AdminWrap = styled.div`
+  @media all and (min-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .box {
+    @media all and (min-width: 768px) {
+      width: 315px;
+      background: #ffffff;
+      box-shadow: 8px 8px 20px rgba(0, 0, 0, 0.05);
+      border-radius: 16px;
+      padding: 40px 30px 0 30px;
+      margin: 0;
+    }
+  }
+`;
 
 export default AdminContainer;
