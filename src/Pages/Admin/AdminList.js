ad5676715d2734bd591a8149c3091980e5df408f
jungeun-p <mrk7753@naver.com>
2021-07-28 16:00:44 +0900

    [ modify ] 회사 등록 신청 리스트 확인
    - admin 기본 상태값 null로 변경
    - AdminList에서 dataList(admin 리덕스)가 null값이 아닐 때 출력 가능

--- src/Pages/Admin/AdminList.js
+++ src/Pages/Admin/AdminList.js
@@ -3,8 +3,20 @@ import { useSelector } from 'react-redux';
 
 const AdminList = () => {
   const { dataList } = useSelector(state => state.admin);
-  dataList.map(it => console.log(it.companyName));
-  return <div></div>;
+  if (!dataList) {
+    console.log('loading...');
+  } else {
+    dataList.map(it => console.log(it.companyName));
+  }
+  return (
+    <>
+      {!dataList ? (
+        <div>loading</div>
+      ) : (
+        <div>{dataList.map(it => it.companyName)}</div>
+      )}
+    </>
+  );
 };
 
 export default AdminList;
