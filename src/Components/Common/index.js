c7807fbb2bc1491ea229b05eaa3b2177b6f1731c
jungeun-p <mrk7753@naver.com>
2021-08-03 18:30:14 +0900

    [ modify ] 배경화면 이미지 추가
    - 웹 환경에서 배경화면 이미지 보이기/앱 환경에서 숨기기

--- src/Components/Common/index.js
+++ src/Components/Common/index.js
@@ -1,20 +1,32 @@
 import styled from 'styled-components';
 import TitleB from './Title';
+import BackgroundImg from '../../assets/Img/background/BackgraoundImg.png';
 // page의 전체를 잡아주는 container
+
 export const ContainerRoot = styled.main`
   position: relative;
   display: flex;
   padding: 0;
   flex-direction: column;
+  top: 0;
   width: 100%;
+  height: 100vh;
   box-sizing: border-box;
   /* margin-left: auto;
   margin-right: auto; */
   flex: 1;
-  /* @media all and (min-width: 768px) {
-    padding: 0 10vw;
-  } */
-  background-color: #ffffff;
+  /* background-image: url(${BackgroundImg});
+  background-repeat: no-repeat;
+  background-size: 30vw auto;
+  background-position: right bottom;
+  background-attachment: fixed; */
+  @media all and (min-width: 768px) {
+    background-size: 25vw auto;
+    background-image: url(${BackgroundImg});
+    background-repeat: no-repeat;
+    background-position: right bottom;
+    background-attachment: fixed;
+  }
 `;
 
 // ContainerRoot 하위 Wrapper
