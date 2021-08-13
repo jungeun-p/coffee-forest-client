9c0dccb7a965345fd2709e322ff29c4f1d482921
jungeun-p <mrk7753@naver.com>
2021-08-13 14:41:21 +0900

    [ modify ] 월간 스케줄에서 이번달 날짜 출력 적용

--- src/Pages/Mypage/Monthly/Plan/WorkPlanList.js
+++ src/Pages/Mypage/Monthly/Plan/WorkPlanList.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import { TitleB } from '../../../../Components/Common';
+import { WorkListArticle } from '../../../../Components/List';
 
 const WorkPlanList = () => {
   return (
     <WorkWrapperList>
       {/* 이번 달 중 오늘 스케줄 */}
       <TitleB>오늘의 일정</TitleB>
+      <WorkListArticle
+        titleType="MEETING"
+        title="개발자 코드리뷰"
+        date="06. 08"
+        time="오전 9 : 30 ~ 오전 11 : 30"
+      />
+      <WorkListArticle
+        titleType="VACATION"
+        title="박지민 휴가"
+        date="06. 08"
+        time="오전 9 : 30 ~ 오전 11 : 30"
+      />
     </WorkWrapperList>
   );
 };
-const WorkWrapperList = styled.div`
-  @media all and (min-width: 768px) {
-    margin-left: 70px;
-  }
-`;
+const WorkWrapperList = styled.div``;
 export default WorkPlanList;
